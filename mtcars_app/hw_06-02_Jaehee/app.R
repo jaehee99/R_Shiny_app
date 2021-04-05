8a93e5e53f694bafee4e5f78a7fbf4fdb2c90b63
Jaehee Lee <jaehee0112@newjoeys.com>
2021-04-05 20:22:03 +0900

    mtcars app update

---
+++ mtcars_app/hw_06-02_Jaehee/app.R
@@ -0,0 +1,27 @@
+library(shiny)
+library(ggplot2)
+library(ggthemes)
+plot_choices = c("Density Plot", "Histogram", "Fruquency Polygon")
+ui <- fluidPage(
+    varSelectInput("var1", "X Variable", data = mtcars),
+    plotOutput("plot"), 
+    radioButtons("Choices", "Choose a plot type", choices = plot_choices),
+    
+)
+
+server <- function(input, output) {
+    output$plot <- renderPlot({
+        ggplot(mtcars, aes(x = !!input$var1)) + 
+            switch( input$Choices,
+                "Histogram"= geom_histogram(),
+                "Density Plot"= geom_density(), 
+                "Fruquency Polygon" = geom_freqpoly()
+                
+            ) +
+            theme_bw()
+        
+
+    })
+}
+
+shinyApp(ui = ui, server = server)

3eff35b3056af7d4da519f4c932fce8a2cef289b
Jaehee Lee <jaehee0112@newjoeys.com>
2021-04-05 20:01:40 +0900

    mpg hw update

---
+++ mpg_app/hw_06-01_Jaehee/app.R
@@ -0,0 +1,18 @@
+library(shiny)
+library(ggplot2)
+
+ui <- fluidPage(
+    varSelectInput("var1", "X Variable", data = mpg, selected = "cty"),
+    varSelectInput("var2", "Y Variable", data = mpg, selected = "hwy"),
+    varSelectInput("var3", "Color Variable", data = mpg, selected = "class"),
+    plotOutput("plot")
+)
+
+server <- function(input, output) {
+    output$plot <- renderPlot({
+        ggplot(mpg, aes(x = !!input$var1, y = !!input$var2, color = !!input$var3)) +
+            geom_point()
+    })
+}
+
+shinyApp(ui, server)

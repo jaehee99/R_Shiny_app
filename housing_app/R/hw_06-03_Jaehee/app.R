61765120c28512eeed82edf45ffed2bb4ae7756d
Jaehee Lee <jaehee0112@newjoeys.com>
2021-04-19 04:47:14 +0900

    Extra credit

--- housing_app/R/hw_06-03_Jaehee/app.R
+++ housing_app/R/hw_06-03_Jaehee/app.R
@@ -265,28 +265,28 @@ server <- function(input, output) {
                     geom_point()
             }
         }
-       else if (is.factor(estate[[input$var1_0]]) && is.numeric(estate[[input$var2_0]])){
+        else if (is.factor(estate[[input$var1_0]]) && is.numeric(estate[[input$var2_0]])){
             if(input$log_2){
                 ggplot(estate, aes(x = !!input$var1_0, y = !!input$var2_0)) + 
                     geom_boxplot() +
                     scale_y_log10()
             }
-           else if(input$log_1){
-                   validate(
-                       need(is.double(estate[[input$var1_0]]),
-                            "not numeric")
-                   )
-           }
-              else{
-                  ggplot(estate, aes(x = !!input$var1_0, y = !!input$var2_0)) + 
-                      geom_boxplot()
-                  }
+            else if(input$log_1){
+                validate(
+                    need(is.double(estate[[input$var1_0]]),
+                         "not numeric")
+                )
+            }
+            else{
+                ggplot(estate, aes(x = !!input$var1_0, y = !!input$var2_0)) + 
+                    geom_boxplot()
+            }
         }
         else if (is.numeric(estate[[input$var1_0]]) && is.factor(estate[[input$var2_0]])){
             if(input$log_1){
                 ggplot(estate, aes(x = !!input$var1_0, y = !!input$var2_0)) + 
                     geom_boxploth() +
-                scale_x_log10()
+                    scale_x_log10()
             }
             else if(input$log_2){
                 validate(
@@ -311,9 +311,24 @@ server <- function(input, output) {
             }
         }
     })
-            output$table <- renderDataTable({
-            keep(estate, ~ typeof(.) == "double")
-        })
+    output$table <- renderDataTable({
+        keep(estate, ~ typeof(.) == "double")
+    })
 }
 shinyApp(ui = ui, server = server)
+
+
+# I completed Extra credit Tab 2  
+
+# Extra credit Webinar
+
+#1. How has the R Studio shiny development changed their focus over the past several years?
+#In the past, there were heavy loads on shiny applications, they monitored the traffic, used hands, and complicated, etc. These Days, they are focusing on making improvements in ui to be more flexible. Also, they are making shiny ready for production and doing automatic testing. They are also improving the speed and they are working on reinventing the space, providing lots of examples and documentation for users. When doing Shiny apps people have to have different perspectives than doing typical R. It is good if you learn how to do software engineering to be more efficient. 
+#Furthermore, RStudio's shiny development is embracing people from a lot of backgrounds. For example, there was a person who came from a designer background and was involved in RStudio Shiny development. This designer perspective helped to visualize better and update the web library, seeing from different perspectives compared to other computer science background people. To sum up, in the past RStudio shiny was complicated and had some traffic, but these days it is making it more interactive to the users and becoming more efficient.
+
+#2. How does your approach to writing shiny apps change when you think they will need to be maintained
+#over the long run by your “future self” or others?
+# If I wrote a shiny app and if I assume that this will need to be maintained over the long run by my “future self” or others, then I will use Github for version control. This is good for managing the whole process and also good for collaboration. Or I can use other version management systems for this Shiny app. This is good because people can go back and do that, several times. This helps to track and manage changes to the code on R shiny app. This will help it to be maintained for over the long run.
+
+
 
